7100bc891ebb20942873d07a4c782270ab9c5f59
Эльвира <sh.irina.nik@mail.ru>
2020-05-04 13:09:31 +0600

    Test

--- src/Interface.java
+++ src/Interface.java
@@ -1,14 +1,13 @@
 import javax.swing.*; //  позволяет графическим интерфейсам Java задействовать графические возможности ос
 import java.awt.*; //обеспечивает взаимодействие с графической подсистемой ос
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class Interface extends JFrame {
 
     public void Draw() { // метод отрисовки графической оболочки
 
         setTitle("SpeedTest program"); // название графического окна
-        setSize(new Dimension(600, 400)); // размер графического окна
+        setSize(600, 400);
         Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = getSize();
         // рассчет положения графического окна по центру экрана
         if (fSize.height > sSize.height) {
@@ -19,52 +18,15 @@ public class Interface extends JFrame {
         }
         setLocation((sSize.width - fSize.width) / 2, (sSize.height - fSize.height) / 2); // вывод окна в указаном месте
         setDefaultCloseOperation(EXIT_ON_CLOSE); // обработка события (закрыть окно)
-        setVisible(true); // сделать окно видимым
 
+        Label d = new Label("Free space after the process: 755 GB" );
+        add(d);
+        setVisible(true);
     }
 
-    public class Test extends JFrame {
-        JButton button;
-        JLabel l1,l2,l3,l4;
-        JTextField t1, t2;
-        String a, b;
-        Runner run = new Runner();
-
-        public Test(String s) {
-            super(s);
-            setLayout(new FlowLayout());
-            button = new JButton("Start");
-
-            l1 = new JLabel("Total size: ");
-            l2 = new JLabel("Space free: ");
-            l3=new JLabel ("");
-            l4=new JLabel ("");
-            t1 = new JTextField(20);
-            t2 = new JTextField(20);
-            add(button);
-            add(l1);
-            add(l2);
-            add(l3);
-            add(l4);
-            add(t1);
-            add(t2);
-            button.addActionListener(run);
-        }
-
-        public class Runner implements ActionListener {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    if (e.getSource() == button)
-
-                        l2.setText(a);
-                    l3.setText(b);
-
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, "Введите в поле число.");
-                }
-
-            }
-        }
-    }
 }
+
+
+
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import java.io.File;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Interface anInterface = new Interface();
+        anInterface.Draw();
         Work work = new Work();
         work.SpaceDisk();
         work.file();
-        //Interface anInterface = new Interface ();
-        // anInterface.Draw();
 
     }
-
 }
+
 
